f62dd3824e7441a1584f49261fdb96f8099c2eb5
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-04-25 00:23:44 +0500

    Changed variables names

--- drawing/draw_array.py
+++ drawing/draw_array.py
@@ -2,9 +2,9 @@ import pygame
 from typing import List
 
 
-def draw_array(array: List[int], rect_size: int, WIDTH: int, HEIGHT: int, screen: pygame.display):
-    x_coord = (WIDTH - rect_size * len(array)) // 2
+def draw_array(array: List[int], rect_size: int, screen_width: int, screen_height: int, screen: pygame.display):
+    x_coord = (screen_width - rect_size * len(array)) // 2
     for el in array:
         pygame.draw.rect(screen, (100, 100, 100), (x_coord,
-                         HEIGHT - el * rect_size, rect_size, el * rect_size))
+                         screen_height - el * rect_size, rect_size, el * rect_size))
         x_coord += rect_size
