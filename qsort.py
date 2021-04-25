b8989bc5cf06d2dafa671f9a33a0205bdb3d392d
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-04-25 13:29:05 +0500

    Added depth parameter to qsort

--- qsort.py
+++ qsort.py
@@ -13,10 +13,10 @@ pygame.display.set_caption("quick sort")
 clock = pygame.time.Clock()
 
 array = [random.randint(0, 30) for x in range(40)]
-array_states = [array.copy()]
+array_states = [[(array.copy(), 0, len(array))]]
 
 
-def qsort(start_index, end_index):
+def qsort(start_index, end_index, depth):
     if end_index - start_index < 2:
         return
     global array
@@ -32,22 +32,28 @@ def qsort(start_index, end_index):
         else:
             more.append(number)
     array[start_index:end_index] = less + equals + more
-    array_states.append(array.copy())
-    qsort(start_index, start_index + len(less))
-    qsort(end_index - len(more), end_index)
+    if len(array_states) == depth:
+        array_states.append([])
+    array_states[depth].append((array.copy(), start_index, end_index))
+    qsort(start_index, start_index + len(less), depth + 1)
+    qsort(end_index - len(more), end_index, depth + 1)
 
 
-qsort(0, len(array))
+qsort(0, len(array), 1)
 state = 0
 FPS = len(array_states) // 6
 if not FPS:
     FPS = 1
+array_to_draw = array_states[state][0][0].copy()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
     screen.fill((255, 255, 255))
-    draw_array(array_states[state], 10, WIDTH, HEIGHT, screen)
+    for el in array_states[state]:
+        (arr, index_start, index_end) = el
+        array_to_draw[index_start:index_end] = arr[index_start:index_end]
+    draw_array(array_to_draw, 10, WIDTH, HEIGHT, screen)
     pygame.display.flip()
     if state < len(array_states) - 1:
         state += 1
